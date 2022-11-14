68a4e6db9fec5f0b05dd629a5095e210453e41ac
dmkrupin <dev.dmkrupin@gmail.com>
2022-11-14 14:32:08 +0000

    Deploying to gh-pages from @ dmkrupin/all4smart@b655f746441ce5076336dee6fd5134d340f9d83c 🚀

--- js/vendor.js
+++ js/vendor.js
@@ -1,11 +1,6 @@
 // Swiper 7.4.1
 import './vendor/swiper.js';
-import {masking} from './vendor/masking-inputs.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-
-
-  masking.init();
-
-
+  
 });
